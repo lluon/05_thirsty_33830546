593d8cf92ff4925c4f5d05a9ab785b9ee1c44064
lluon <lluon001gold.ac.uk>
2025-10-29 15:32:03 +0000

    v1

---
+++ routes/main.js
@@ -0,0 +1,9 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+
+// Handle the main routes
+// TODO
+
+// Export the router object so index.js can access it
+module.exports = router;

b96d01f89b62de6bea6f3ad2775ae8a05fc30070
lluon <lluon001gold.ac.uk>
2025-10-29 16:05:10 +0000

    v1

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 // Handle the main routes
-// TODO
+router.get("/", (req, res) => {
+    res.render("index.ejs")
+
+}); 
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs")
+}); 
+
+router.get("/search", (req, res) => {
+    res.render("search.ejs")
+}); 
 
 // Export the router object so index.js can access it
 module.exports = router;

8cc3008c11b500a4550a9a20146e79ac096b8dfc
lluon <lluon001@gold.ac.uk>
2025-10-29 21:51:39 +0000

    part a

--- routes/main.js
+++ routes/main.js
@@ -3,18 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 // Handle the main routes
-router.get("/", (req, res) => {
-    res.render("index.ejs")
+// Create a new router
+const express = require("express");
+const router = express.Router();
 
-}); 
+// Handle the main routes
+router.get("/", (req, res) => {
+    res.render("index.ejs")}); 
 
 router.get("/about", (req, res) => {
-    res.render("about.ejs")
-}); 
-
-router.get("/search", (req, res) => {
-    res.render("search.ejs")
-}); 
-
+    res.render("about.ejs")}); 
+    
 // Export the router object so index.js can access it
 module.exports = router;

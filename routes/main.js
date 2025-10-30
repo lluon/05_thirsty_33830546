60828e815325452d5ba5243191e97649b976188d
lluon <lluon001@gold.ac.uk>
2025-10-30 12:29:13 +0000

    task_3

--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,19 @@
 // Create a new router
 const express = require("express");
 const router = express.Router();
+// Define our data
+var shopData = {shopName: "osteria tufo"};
 
 // Handle the main routes
 router.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs",shopData)
 }); 
 
 router.get("/about", (req, res) => {
-    res.render("about.ejs")
+    res.render("about.ejs",shopData)
 }); 
 router.get("/search", (req, res) => {
-    res.render("search.ejs")
+    res.render("search.ejs",shopData)
 }); 
 
 // Export the router object so index.js can access it

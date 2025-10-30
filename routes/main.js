8f8cb4d818c808d7e8b7e7ae7c70fba7872640c9
lluon <lluon001@gold.ac.uk>
2025-10-30 11:34:22 +0000

    downgrade to version 4 express

--- routes/main.js
+++ routes/main.js
@@ -3,16 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 // Handle the main routes
-// Create a new router
-const express = require("express");
-const router = express.Router();
-
-// Handle the main routes
 router.get("/", (req, res) => {
-    res.render("index.ejs")}); 
+    res.render("index.ejs")
+}); 
 
 router.get("/about", (req, res) => {
-    res.render("about.ejs")}); 
-    
+    res.render("about.ejs")
+}); 
+router.get("/search", (req, res) => {
+    res.render("search.ejs")
+}); 
+
 // Export the router object so index.js can access it
 module.exports = router;
+
